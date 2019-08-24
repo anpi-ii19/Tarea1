2137e30d540ea751b19905cd4d26e8a095bf8af2
Arturo Cordoba <arturocv16@gmail.com>
2019-08-24 13:29:10 -0600

    Cambio comentarios graficar_error Python

--- python/graficar_error.py
+++ python/graficar_error.py
@@ -1,13 +1,12 @@
 from matplotlib import pyplot as plt
 
-"""
-Funcion para realizar la grafica de error, iteraciones vs | f(xk) |
-:param eje_x: lista con todos los valores que deben graficarse en el eje x
-:param eje_y: lista con todos los valores que deben graficarse en el eje y
-"""
-
 
 def graficar_error(eje_x, eje_y):
+    """
+    Funcion para realizar la grafica de error, iteraciones vs | f(xk) |
+    :param eje_x: lista con todos los valores que deben graficarse en el eje x
+    :param eje_y: lista con todos los valores que deben graficarse en el eje y
+    """
     plt.plot(eje_x, eje_y)  # Se asignan los valores a los ejes de coordenadas
     plt.xlabel('Iteracion')  # Se le coloca un nombre al eje x
     plt.ylabel('| f(xk) |')  # Se le coloca un nombre al eje y
