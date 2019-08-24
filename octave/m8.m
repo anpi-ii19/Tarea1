612252cd12ed0b0959f9d5483a8eb1b05903883c
Fagonar96 <fagonar96@gmail.com>
2019-08-24 14:02:55 -0600

    Implementacion del metodo M8 en Octave

--- octave/m8.m
+++ octave/m8.m
@@ -53,99 +53,95 @@ function [x_aprox, iter] = m8(func, xk, y, tol, graph)
     return
   endif
   
-  try
-    # Variable que contiene la conversión de la ecuación simbólica
-    ec = matlabFunction(sym(func))
-    # Variable que contiene la derivada de la función
-    dec = matlabFunction(diff(sym(func)))
+  
+  % Variable que contiene la conversión de la ecuación simbólica
+  ec = matlabFunction(sym(func))
+  % Variable que contiene la derivada de la función
+  dec = matlabFunction(diff(sym(func)))
 
-    # Listas donde se guardan los valores para graficar el error
-    lista_fxk = []
-    lista_iter = []
+  % Listas donde se guardan los valores para graficar el error
+  lista_fxk = []
+  lista_iter = []
+  
+  % Se inicializa el contador de iteraciones
+  ite = 0
+  
+  while 1
+    % Variable que contiene la imagen de xk en la función
+    eval_ec = ec(xk)
     
-    # Se inicializa el contador de iteraciones
-    ite = 0
-    
-    while 1
-      # Variable que contiene la imagen de xk en la función
-      eval_ec = ec(xk)
-      
-      # Se guardan los valores para la grafica
-      if graph == 1
-        lista_fxk = [lista_fxk abs(eval_ec)]
-        lista_iter = [lista_iter (ite)]
-      endif
-      
-      # Verificar la condición de parada si la imagen del xk es menor o
-      # igual que la tolerancia
-      if abs(eval_ec) <= tol
-        break
-      # Si NO se cumple la condición de parada
-      else
-        # Se calcula el wk y su imagen en la función
-        wk = xk + (y * eval_ec)
-        eval_wec = ec(wk)
-
-        # Verificar que no se indefina yk
-        if (eval_wec - eval_ec) == 0
-            disp("El yk se indefine")
-            return
-        endif
-            
-        # Se calcula el yk y su imagen en la función
-        yk = xk - (y * (eval_ec ** 2 / (eval_wec - eval_ec)))
-        eval_yec = ec(yk)
-
-        # Se calcula el zk y su imagen en la función
-        z1 = ((wk - yk) / ((xk - yk) * y))
-        z2 = ((xk - yk) / ((wk - yk) * y * eval_ec)) * eval_wec
-        z3 = ((wk + xk - 2 * yk) / ((xk - yk) * (wk - yk))) * eval_yec
-        
-        # Verificar que no se indefina zk
-        if (z1 - z2 - z3) == 0
-            disp("El zk se indefine")
-            return
-        endif
-        
-        zk = yk - (eval_yec / (z1 - z2 - z3))
-        eval_zec = ec(zk)
-
-        # Se calcula el xk+1 para la siguiente iteración
-        h1 = (((yk - zk) * (wk - zk))) / ((xk - zk) * y * (xk - yk))
-        h2 = ((((yk - zk) * (xk - zk)) / ((wk - zk) * (wk - yk) * (wk * xk)))
-              * eval_wec)
-        h3 = (((wk * xk) - (wk * xk) - (xk * zk) + zk ** 2) /
-              ((yk - wk) * (yk - xk) * (yk - zk))) * eval_yec
-        h4 = (((wk * xk) + (wk * yk) - (2 * wk * zk) + (xk * yk) - (2 * xk * zk) -
-               (2 * yk * zk) + (3 * zk ** 2)) / ((zk - wk) * (zk - xk) * (zk - yk)) *
-              eval_zec)
-        # Verificar que no se indefina el zk
-        if (h1 + h2 + h3 + h4) == 0
-            disp("El xk+1 se indefine")
-            return
-        endif
-        
-        # Se calcula el xk+1 para la siguiente iteración
-        xk = zk - (eval_zec / (h1 + h2 + h3 + h4))
-        # Se añade una iteración
-        ite = ite + 1
-      endif
-    endwhile
-    
-    # Si termina el ciclo, grafica el error y retorna el resultado
+    % Se guardan los valores para la grafica
     if graph == 1
-      graficar_error(lista_iter, lista_fxk)
+      lista_fxk = [lista_fxk abs(eval_ec)]
+      lista_iter = [lista_iter (ite)]
     endif
     
-    x_aprox =  xk
-    iter = ite
+    % Verificar la condición de parada si la imagen del xk es menor o
+    % igual que la tolerancia
+    if abs(eval_ec) <= tol
+      break
+    % Si NO se cumple la condición de parada
+    else
+      % Se calcula el wk y su imagen en la función
+      wk = xk + (y * eval_ec)
+      eval_wec = ec(wk)
+
+      % Verificar que no se indefina yk
+      if (eval_wec - eval_ec) == 0
+          disp("El yk se indefine")
+          return
+      endif
+          
+      % Se calcula el yk y su imagen en la función
+      yk = xk - (y * (eval_ec ** 2 / (eval_wec - eval_ec)))
+      eval_yec = ec(yk)
+
+      % Se calcula el zk y su imagen en la función
+      z1 = ((wk - yk) / ((xk - yk) * y))
+      z2 = ((xk - yk) / ((wk - yk) * y * eval_ec)) * eval_wec
+      z3 = ((wk + xk - 2 * yk) / ((xk - yk) * (wk - yk))) * eval_yec
+      
+      % Verificar que no se indefina zk
+      if (z1 - z2 - z3) == 0
+          disp("El zk se indefine")
+          return
+      endif
+      
+      zk = yk - (eval_yec / (z1 - z2 - z3))
+      eval_zec = ec(zk)
+
+      % Se calcula el xk+1 para la siguiente iteración
+      h1 = (((yk - zk) * (wk - zk))) / ((xk - zk) * y * (xk - yk))
+      h2 = ((((yk - zk) * (xk - zk)) / ((wk - zk) * (wk - yk) * (wk * xk)))
+            * eval_wec)
+      h3 = (((wk * xk) - (wk * xk) - (xk * zk) + zk ** 2) /
+            ((yk - wk) * (yk - xk) * (yk - zk))) * eval_yec
+      h4 = (((wk * xk) + (wk * yk) - (2 * wk * zk) + (xk * yk) - (2 * xk * zk) -
+             (2 * yk * zk) + (3 * zk ** 2)) / ((zk - wk) * (zk - xk) * (zk - yk)) *
+            eval_zec)
+      % Verificar que no se indefina el zk
+      if (h1 + h2 + h3 + h4) == 0
+          disp("El xk+1 se indefine")
+          return
+      endif
+      
+      % Se calcula el xk+1 para la siguiente iteración
+      xk = zk - (eval_zec / (h1 + h2 + h3 + h4))
+      % Se añade una iteración
+      ite = ite + 1
+    endif
+  endwhile
     
-  catch
-    disp("Sintaxis de la funcion es incorrecta")
-    return
-  end_try_catch
+  % Si termina el ciclo, grafica el error y retorna el resultado
+  if graph == 1
+    graficar_error(lista_iter, lista_fxk)
+  endif
+  
+  x_aprox =  xk
+  iter = ite
+    
 endfunction
 
-#Ejemplo de prueba para el método M8
+% Ejemplo de prueba para el método M8
 g = '(cos(2*x))**2 - (x**2)'
 print(m8(g, 3/4, 1, 10**-5, 1))
