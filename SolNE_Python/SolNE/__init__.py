f9927db7d44439d05534dddc796520edb7ab8697
Arturo Cordoba <arturocv16@gmail.com>
2019-08-25 15:31:43 -0600

    Import de los metodos en el __init__.py paquete Python

---
+++ SolNE_Python/SolNE/__init__.py
@@ -0,0 +1,9 @@
+# Se importan los metodos que utilizan derivadas
+from .ud import sne_ud_1
+from .ud import sne_ud_2
+from .ud import sne_ud_3
+from .ud import sne_ud_4
+
+# Se importan los metodos que no utilizan derivadas
+from .fd import sne_fd_1
+from .fd import sne_fd_2
