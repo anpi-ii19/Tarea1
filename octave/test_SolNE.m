e16dc534065d85d0d4973340e059a1d72c28fe7d
Joseph Vargas <joseph@cpokemon.com>
2019-08-25 20:22:43 -0600

    Agregados tests de octave

--- octave/test_SolNE.m
+++ octave/test_SolNE.m
@@ -27,6 +27,18 @@ funcion3 = 'cos(x) - x'
 %sne_ud_4(funcion3, 3/4, 10^-5, 0)
 %
 %
+%% Ejemplo de prueba para el metodo de Darvishi - Barati
+%sne_ud_5(funcion1, 2/4, 10^-5, 0)
+%sne_ud_5(funcion2, 2/4, 10^-5, 0)
+%sne_ud_5(funcion3, 3/4, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo de Darvishi - Barati
+%sne_ud_6(funcion1, 2/4, 10^-5, 0)
+%sne_ud_6(funcion2, 2/4, 10^-5, 0)
+%sne_ud_6(funcion3, 3/4, 10^-5, 0)
+%
+%
 %% Ejemplo de prueba para el metodo de Steffensen
 %sne_fd_1(funcion1, 5/7, 10^-5, 0)
 %sne_fd_1(funcion1, 5/7, 10^-5, 0)
@@ -37,3 +49,11 @@ funcion3 = 'cos(x) - x'
 %sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
 %sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
 %sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo M8
+%sne_fd_3(funcion1, 5/7, 3/4, 10^-5, 0)
+%sne_fd_3(funcion1, 5/7, 3/4, 10^-5, 0)
+%sne_fd_3(funcion1, 5/7, 3/4, 10^-5, 0)
+%
+%
