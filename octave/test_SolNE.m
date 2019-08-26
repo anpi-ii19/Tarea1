fb1cf511828df84d6e4d108123f5b3e18f9bbf36
Arturo Cordoba <arturocv16@gmail.com>
2019-08-25 19:47:51 -0600

    Archivo de pruebas para las funciones en Octave

--- octave/test_SolNE.m
+++ octave/test_SolNE.m
@@ -1,22 +1,39 @@
 pkg load symbolic
 
-%Weerakoon_Fernando
-funcion1 = 'cos(2*x)^2 - x^2';
-%sne_ud_1(funcion1, 3/4, 10^-5);
+funcion1 = 'cos(2*x)^2 - x^2'
+funcion2 = 'exp(x) - x - 2'
+funcion3 = 'cos(x) - x'
 
-funcion2 = 'exp(x) - x^3 - x';
-%sne_ud_1(funcion2, 3/4, 10^-5);
-
-
-%Ejemplo de prueba para el metodo de Chun-Kim
+%% Ejemplo de prueba para el metodo de Weerakoon_Fernando
+%sne_ud_1(funcion1, 3/4, 10^-5, 0)
+%sne_ud_1(funcion2, 3/4, 10^-5, 0)
+%sne_ud_1(funcion3, 3/4, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo de Chun-Kim
 %sne_ud_2(funcion1, 3/4, 10^-5, 0)
 %sne_ud_2(funcion2, 3/4, 10^-5, 0)
-
-
-% Ejemplo de prueba para el metodo de Ozban-Homeier
-%sne_ud_3(funcion1, 3/4, 10^-5, 1)
-%sne_ud_3(funcion2, 3/4, 10^-5, 1)
-
-%Darvishi - Barati
-%sne_ud_4(funcion1, 1, 10 ^ -5)
-%sne_ud_4(funcion2, 2 / 4, 10 ^ -5);
+%sne_ud_2(funcion3, 3/4, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo de Ozban-Homeier
+%sne_ud_3(funcion1, 3/4, 10^-5, 0)
+%sne_ud_3(funcion2, 3/4, 10^-5, 0)
+%sne_ud_3(funcion3, 3/4, 10^-5, 0)
+%
+%% Ejemplo de prueba para el metodo de Darvishi - Barati
+%sne_ud_4(funcion1, 2/4, 10^-5, 0)
+%sne_ud_4(funcion2, 2/4, 10^-5, 0)
+%sne_ud_4(funcion3, 3/4, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo de Steffensen
+%sne_fd_1(funcion1, 5/7, 10^-5, 0)
+%sne_fd_1(funcion1, 5/7, 10^-5, 0)
+%sne_fd_1(funcion1, 5/7, 10^-5, 0)
+%
+%
+%% Ejemplo de prueba para el metodo M8
+%sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
+%sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
+%sne_fd_2(funcion1, 5/7, 3/4, 10^-5, 0)
